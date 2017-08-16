f8fbc4d037d1b432e551e375531ad05b80a6b222
조성현 <seonghyun.cho@navercorp.com>
2017-08-16 13:20:56 +0900

    Initial Commit

--- ShareWithInstagram/ViewController.swift
+++ ShareWithInstagram/ViewController.swift
@@ -9,17 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var documentsInteractionsController: UIDocumentInteractionController!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func share(_ sender: UIButton) {
+//        presentActivityViewController()
+        shareOnInstagram()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func presentActivityViewController() {
+        let activityViewController = UIActivityViewController(activityItems: ["Title"], applicationActivities: nil)
+        
+        present(activityViewController, animated: true, completion: nil)
     }
-
-
+    
+    private func shareOnInstagram() {
+        guard let image = imageView.image else { return }
+        
+        shareOnInstagram(image, text: "텍스트")
+    }
+    
 }
 
+extension ViewController: UIDocumentInteractionControllerDelegate {
+    func shareOnInstagram(_ photo: UIImage, text: String?) {
+        let instagramUrl = URL(string: "instagram://app")!
+        
+        if UIApplication.shared.canOpenURL(instagramUrl) {
+            let imageData = UIImageJPEGRepresentation(photo, 1.0)!
+            let captionString = text ?? ""
+            
+            let writePath = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("instagram.ig")
+            do {
+                try imageData.write(to: writePath)
+                documentsInteractionsController = UIDocumentInteractionController(url: writePath)
+                documentsInteractionsController.delegate = self
+                documentsInteractionsController.uti = "com.instagram.photo"
+                documentsInteractionsController.annotation = ["InstagramCaption": captionString]
+                documentsInteractionsController.presentOpenInMenu(from: CGRect.zero, in: self.view, animated: true)
+            }catch {
+                return
+            }
+        } else {
+            let alertController = UIAlertController(title: "Install Instagram", message: "You need Instagram app installed to use this feature. Do you want to install it now?", preferredStyle: .alert)
+            let installAction = UIAlertAction(title: "Install", style: .default, handler: { (action) in
+                //redirect to instagram
+            })
+            alertController.addAction(installAction)
+            
+            let laterAction = UIAlertAction(title: "Later", style: .cancel, handler: nil)
+            alertController.addAction(laterAction)
+            
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+}
